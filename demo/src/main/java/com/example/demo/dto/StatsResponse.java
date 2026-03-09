a9587afa2997f57003fbb21174e84bdeb6ce498f
azfrk01 <azfarkhan8372@gmail.com>
2026-03-09 14:33:03 +0530

    done with pagination to getAllStudents

--- demo/src/main/java/com/example/demo/dto/StatsResponse.java
+++ demo/src/main/java/com/example/demo/dto/StatsResponse.java
@@ -1,34 +1,26 @@
 package com.example.demo.dto;
-
 import java.util.Map;
 
-public class StatsResponse {
-
+public class StatsResponse{
     private long totalStudents;
     private Map<String, Long> branchCounts;
     private Map<Integer, Long> yearCounts;
-
-    public long getTotalStudents() {
+    public long getTotalStudents(){
         return totalStudents;
     }
-
-    public void setTotalStudents(long totalStudents) {
+    public void setTotalStudents(long totalStudents){
         this.totalStudents = totalStudents;
     }
-
-    public Map<String, Long> getBranchCounts() {
+    public Map<String, Long> getBranchCounts(){
         return branchCounts;
     }
-
-    public void setBranchCounts(Map<String, Long> branchCounts) {
+    public void setBranchCounts(Map<String, Long> branchCounts){
         this.branchCounts = branchCounts;
     }
-
-    public Map<Integer, Long> getYearCounts() {
+    public Map<Integer, Long> getYearCounts(){
         return yearCounts;
     }
-
-    public void setYearCounts(Map<Integer, Long> yearCounts) {
+    public void setYearCounts(Map<Integer, Long> yearCounts){
         this.yearCounts = yearCounts;
     }
 }
